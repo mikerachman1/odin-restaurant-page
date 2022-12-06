f40080c6ba0e5db14b3e6860e78f89a75f85f7f0
Mike Rachman <mikerachman1@gmail.com>
2022-12-06 15:44:57 -0500

    tabSwitcher function written in index.js that updates dom with different modules

--- src/index.js
+++ src/index.js
@@ -3,4 +3,20 @@ import homeTab from "./home";
 import menuTab from "./menu";
 import contactTab from "./contact";
 
-contactTab();
+const tabSwitcher = (() => {
+  const homeBtn = document.getElementById("home");
+  const menuBtn = document.getElementById("menu");
+  const contactBtn = document.getElementById("contact");
+
+  const switchTabs = (tabFunction) => {
+    const contentDiv = document.getElementById("content");
+    contentDiv.innerHTML = '';
+    tabFunction();
+  }
+
+  menuBtn.addEventListener('click', () => {switchTabs(menuTab)});
+  contactBtn.addEventListener('click', () => {switchTabs(contactTab)});
+  homeBtn.addEventListener('click', () => {switchTabs(homeTab)});
+
+  homeTab();
+})();
