d29daceb7a698faf5a468c474f48e7579cba3e6b
Mike Rachman <mikerachman1@gmail.com>
2022-12-07 13:30:27 -0500

    styles written, additional classes added to html

--- src/contact.js
+++ src/contact.js
@@ -1,10 +1,12 @@
 export default function contactTab() {
   const contentDiv = document.getElementById("content");
 
-  const str = `<h1>Get In Touch</h1>
-                  <p>We would love to hear from you! Give us a call or send us an email.<p>
-                  <p>Phone: (444)-323-6666</p>
-                  <p>Email: maggiespp@yummy.com</p>`
+  const str = `<h1><u>Get In Touch</u></h1>
+                  <div class="container">
+                    <p>We would love to hear from you! Give us a call or send us an email.<p>
+                    <p>Phone: (444)-323-6666</p>
+                    <p>Email: maggiespp@yummy.com</p>
+                  </div>`
 
   contentDiv.innerHTML = str;
 }
--- src/menu.js
+++ src/menu.js
@@ -1,21 +1,23 @@
 export default function menuTab() {
   const contentDiv = document.getElementById("content");
 
-  const str = `<h1>Menu</h1>
-                  <h3>Drinks</h3>
-                  <p>Coffee - $2.00</p>
-                  <p>Tea - $1.50</p>
-                  <p>Juice - $2.25</p>
-                  <h3>Pancakes</h3>
-                  <p>Plain Buttermilk - $8.00</p>
-                  <p>Oatmeal Buttermilk - $8.50</p>
-                  <p>Buckwheat - $9.00</p>
-                  <p>Whole Wheat - $8.00</p>
-                  <p>Blueberry - $9.50</p>
-                  <h3>Other Favorites</h3>
-                  <p>Eggs Benedict - $12.00</p>
-                  <p>Steak and Eggs - $15.00</p>
-                  <p>Build Your Own Omelet - $10.00</p>`
+  const str = `<h1><u>Menu</u></h1>
+                  <div class="container">
+                    <h3><u>Drinks</u></h3>
+                    <p>Coffee - $2.00</p>
+                    <p>Tea - $1.50</p>
+                    <p>Juice - $2.25</p>
+                    <h3><u>Pancakes</u></h3>
+                    <p>Plain Buttermilk - $8.00</p>
+                    <p>Oatmeal Buttermilk - $8.50</p>
+                    <p>Buckwheat - $9.00</p>
+                    <p>Whole Wheat - $8.00</p>
+                    <p>Blueberry - $9.50</p>
+                    <h3><u>Other Favorites</u></h3>
+                    <p>Eggs Benedict - $12.00</p>
+                    <p>Steak and Eggs - $15.00</p>
+                    <p>Build Your Own Omelet - $10.00</p>
+                  </div>`
 
   contentDiv.innerHTML = str;
 }
